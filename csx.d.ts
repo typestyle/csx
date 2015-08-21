31a18b55eb65ab64df2c3a8a7684c557d0396eda
Basarat Syed <basaratali@gmail.com>
2015-08-21 15:42:35 +1000

    ts redux redux

--- csx.d.ts
+++ csx.d.ts
@@ -1,126 +1,126 @@
 declare module "csx" {
-export declare function extend(...args: any[]): {};
-export declare var flexRoot: {
-    display: string;
-};
-export declare var horizontal: any, vertical: any, horizontalReverse: any, verticalReverse: any;
-export declare var wrap: {
-    flexWrap: string;
-};
-export declare var wrapReverse: {
-    flexWrap: string;
-};
-export declare var flexAuto: {
-    flex: string;
-};
-export declare var flexNone: {
-    flex: string;
-};
-export declare var flex: {
-    flex: number;
-};
-export declare var flex1: {
-    flex: number;
-};
-export declare var flex2: {
-    flex: number;
-};
-export declare var flex3: {
-    flex: number;
-};
-export declare var flex4: {
-    flex: number;
-};
-export declare var flex5: {
-    flex: number;
-};
-export declare var flex6: {
-    flex: number;
-};
-export declare var flex7: {
-    flex: number;
-};
-export declare var flex8: {
-    flex: number;
-};
-export declare var flex9: {
-    flex: number;
-};
-export declare var flex10: {
-    flex: number;
-};
-export declare var flex11: {
-    flex: number;
-};
-export declare var flex12: {
-    flex: number;
-};
-export declare var start: {
-    alignItems: string;
-};
-export declare var center: {
-    alignItems: string;
-};
-export declare var end: {
-    alignItems: string;
-};
-export declare var startJustified: {
-    justifyContent: string;
-};
-export declare var centerJustified: {
-    justifyContent: string;
-};
-export declare var endJustified: {
-    justifyContent: string;
-};
-export declare var aroundJustified: {
-    justifyContent: string;
-};
-export declare var betweenJustified: {
-    justifyContent: string;
-};
-export declare var centerCenter: {};
-export declare var selfStart: {
-    alignSelf: string;
-};
-export declare var selfCenter: {
-    alignSelf: string;
-};
-export declare var selfEnd: {
-    alignSelf: string;
-};
-export declare var selfStretch: {
-    alignSelf: string;
-};
-export declare var block: {
-    display: string;
-};
-export declare var hidden: {
-    display: string;
-};
-export declare var invisible: {
-    visibility: string;
-};
-export declare var relative: {
-    position: string;
-};
-export declare var fit: {
-    position: string;
-    top: number;
-    right: number;
-    bottom: number;
-    left: number;
-};
-export declare var fullBleedBody: {
-    margin: number;
-    height: string;
-};
-export declare var scroll: {
-    'webkitOverflowScrolling': string;
-    overflow: string;
-};
-export declare var fixed: {
-    position: string;
-};
-export declare var fixedBottom: any, fixedLeft: any, fixedRight: any, fixedTop: any;
+    export function extend(...args: any[]): {};
+    export var flexRoot: {
+        display: string;
+    };
+    export var horizontal: any, vertical: any, horizontalReverse: any, verticalReverse: any;
+    export var wrap: {
+        flexWrap: string;
+    };
+    export var wrapReverse: {
+        flexWrap: string;
+    };
+    export var flexAuto: {
+        flex: string;
+    };
+    export var flexNone: {
+        flex: string;
+    };
+    export var flex: {
+        flex: number;
+    };
+    export var flex1: {
+        flex: number;
+    };
+    export var flex2: {
+        flex: number;
+    };
+    export var flex3: {
+        flex: number;
+    };
+    export var flex4: {
+        flex: number;
+    };
+    export var flex5: {
+        flex: number;
+    };
+    export var flex6: {
+        flex: number;
+    };
+    export var flex7: {
+        flex: number;
+    };
+    export var flex8: {
+        flex: number;
+    };
+    export var flex9: {
+        flex: number;
+    };
+    export var flex10: {
+        flex: number;
+    };
+    export var flex11: {
+        flex: number;
+    };
+    export var flex12: {
+        flex: number;
+    };
+    export var start: {
+        alignItems: string;
+    };
+    export var center: {
+        alignItems: string;
+    };
+    export var end: {
+        alignItems: string;
+    };
+    export var startJustified: {
+        justifyContent: string;
+    };
+    export var centerJustified: {
+        justifyContent: string;
+    };
+    export var endJustified: {
+        justifyContent: string;
+    };
+    export var aroundJustified: {
+        justifyContent: string;
+    };
+    export var betweenJustified: {
+        justifyContent: string;
+    };
+    export var centerCenter: {};
+    export var selfStart: {
+        alignSelf: string;
+    };
+    export var selfCenter: {
+        alignSelf: string;
+    };
+    export var selfEnd: {
+        alignSelf: string;
+    };
+    export var selfStretch: {
+        alignSelf: string;
+    };
+    export var block: {
+        display: string;
+    };
+    export var hidden: {
+        display: string;
+    };
+    export var invisible: {
+        visibility: string;
+    };
+    export var relative: {
+        position: string;
+    };
+    export var fit: {
+        position: string;
+        top: number;
+        right: number;
+        bottom: number;
+        left: number;
+    };
+    export var fullBleedBody: {
+        margin: number;
+        height: string;
+    };
+    export var scroll: {
+        'webkitOverflowScrolling': string;
+        overflow: string;
+    };
+    export var fixed: {
+        position: string;
+    };
+    export var fixedBottom: any, fixedLeft: any, fixedRight: any, fixedTop: any;
 }
