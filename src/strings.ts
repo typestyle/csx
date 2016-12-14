cfd1fc39db457547e01282d21f1462e5c66b2481
Christopher Wallis <supercryptonite@gmail.com>
2016-12-14 00:17:51 -0500

    fixed quote noop, started important

--- src/strings.ts
+++ src/strings.ts
@@ -1,3 +1,24 @@
+import { CSSHelper } from './interfaces';
+
 export function quote(val: string): string {
-    return `${val}`;
+    return `'${val}'`;
+}
+
+export function important<T extends string>(val: number | string | CSSHelper<T>): string | CSSHelper<T> {
+    if (!val && val !== 0) {
+        return '';
+    }
+    if (typeof (val as CSSHelper<T>).type === 'string') {
+        const helper = val as CSSHelper<T>;
+        return {
+            type: helper.type,
+            toString() {
+                return `${helper.toString()} !important`;
+            }
+        };
+    }
+    // if (typeof val === 'number') {
+    //     return `${val}px !important`;
+    // }
+    return `${val.toString()} !important`;
 }
--- src/tests/strings.ts
+++ src/tests/strings.ts
@@ -4,11 +4,16 @@ import * as assert from 'assert';
 describe("csx/quote", () => {
   it("quotes ' correctly", () => {
       const contraction = csx.quote("it's is a contraction");
-      assert.equal(contraction, 'it\'s is a contraction');
+      assert.equal(contraction, `'it\'s is a contraction'`);
   }); 
 
   it("quotes \" correctly", () => {
       const title = csx.quote(`A title "Should be in Quotes"`);
-      assert.equal(title, 'A title \"Should be in Quotes\"');
+      assert.equal(title, `'A title \"Should be in Quotes\"'`);
+  }); 
+    
+   it("quotes around the string", () => {
+      const title = csx.quote(`Hello, World!`);
+      assert.equal(title, `'Hello, World!'`);
   }); 
 })
