1d3c19ec78423ab9d7a5b901a620224f553bae36
Christopher Wallis <supercryptonite@gmail.com>
2016-12-14 13:46:09 -0500

    modified quote tests

--- src/tests/strings.ts
+++ src/tests/strings.ts
@@ -4,12 +4,12 @@ import * as assert from 'assert';
 describe("csx/quote", () => {
     it("quotes ' correctly", () => {
         const contraction = csx.quote("it's is a contraction");
-        assert.equal(contraction, `'it\'s is a contraction'`);
+        assert.equal(contraction, "'it\'s is a contraction'");
     });
 
     it("quotes \" correctly", () => {
-        const title = csx.quote(`A title "Should be in Quotes"`);
-        assert.equal(title, `'A title \"Should be in Quotes\"'`);
+        const title = csx.quote('A title "Should be in Quotes"');
+        assert.equal(title, "'A title \"Should be in Quotes\"'");
     });
     
     it("quotes around the string", () => {
